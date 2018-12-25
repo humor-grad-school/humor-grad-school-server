166601453ca9eddbc4fed8aae056bb94a376f4a6
skatpgusskat <skatpgusskat@naver.com>
2018-12-26 02:10:47 +0900

    Add best board

--- src/Api/BoardApiRouter.ts
+++ src/Api/BoardApiRouter.ts
@@ -1,19 +1,36 @@
 import Router from 'koa-router';
 import BoardModel from '@/Model/BoardModel';
 import { passAuthorizationMiddleware } from './AuthorizationPassService';
+import PostModel from '@/Model/PostModel';
 
 const router = new Router();
 
 const PAGE_SIZE = 20;
+const MINIMUM_BEST_LIKES = 10;
 
 async function getBoardWithPosts(boardName: string, page: number, pageSize: number) {
+  const isBestBoard = boardName === 'best';
+
+  if (isBestBoard) {
+    const { results: bestPosts } = await PostModel.query()
+      .where('likes', '>=', MINIMUM_BEST_LIKES)
+      .orderBy('id')
+      .page(page, pageSize);
+
+    return {
+      posts: bestPosts,
+    };
+  }
+
   return await BoardModel.query()
     .where({ name: boardName })
     .first()
     .eager('posts')
-  // .modifyEager('posts', builder => {
-  //   builder.orderBy('id').page(page, pageSize);
-  // });
+    .modifyEager('posts', builder => {
+      builder.where('likes', '<', MINIMUM_BEST_LIKES)
+        .orderBy('id')
+        .page(page, pageSize);
+    });
 }
 
 router.get(['/:boardName', '/:boardName/page/:page'], passAuthorizationMiddleware, async ctx => {
